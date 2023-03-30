d125e81400621133ba1264c09103f252f0d5c110
loc_le <lle24@terpmail.umd.edu>
2023-03-30 18:32:16 -0400

    Adding new routes. creating parameter. creating forms. shortening code.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,21 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/FavoriteCourse')
+def favorite_course():
+    print('BMGT' + request.args.get('subject'))
+    print('407' + request.args.get('Course_Number'))
+    return render_template('FavoriteCourse.html')
+
+@app.route('/contact' , methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
